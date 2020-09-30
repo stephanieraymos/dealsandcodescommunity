e18b41ad3b098936fa5bec12ebb4078adda5319b
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2020-09-29 17:10:18 -0700

    Setting up Seller Terms

---
+++ src/SellerTerms.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function SellerTerms() {
+    return (
+        <div>
+            <button onClick="">Amazon Seller</button>
+            <button onClick="">Non-Amazon Seller</button>
+        </div>
+    )
+}
