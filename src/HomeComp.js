6ad23c15a29556351eb7ca0a249df2be314d5940
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2020-10-03 20:48:22 -0700

    Building out structure

---
+++ src/HomeComp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Deals from './DealsComp'
+
+export class HomeComp extends Component {
+    render() {
+        return (
+            <div>
+                <Deals />
+            </div>
+        )
+    }
+}
+
+export default HomeComp
