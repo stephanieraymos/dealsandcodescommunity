bb210685ed5cdca4bac651adca6098ad88c602c1
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2020-10-06 18:37:49 -0700

    Base

---
+++ src/LoginComp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LoginComp() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/ProfileComp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ProfileComp extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ProfileComp
