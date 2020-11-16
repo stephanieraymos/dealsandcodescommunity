212a3382b88216bc7e8b9e4c11a518a2ae52c9cf
Stephanie Raymos <stephanieraymos@gmail.com>
2020-11-15 19:16:18 -0800

    Added photos

--- src/DealsComp.js
+++ src/DealsComp.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
+import bottle from './img/bottle.jpg'
+import mat from './img/mat.jpg'
+import ss from './img/ss.jpg'
+import ts from './img/ts.jpg'
 
 
-const Deal = ({link, title, admin, discount}) => {
+
+const Deal = ({link, image, title, admin, discount}) => {
     return(
         <section className="deal-section">
             <p>{link}</p>
+            <img src={image} alt="Deal"/>
             <h2>{title}</h2>
             <p>Posted By: {admin}</p>
             <p>Total discount: {discount}%</p>
@@ -18,21 +24,31 @@ const DealsComp = () => {
     <div>
         <Deal 
             link="Link"
+            image={bottle}
             title="Water Bottle"
             admin="Stephanie Raymos"
             discount={50}
         />
         <Deal 
             link="Link"
+            image={mat}
             title="Yoga Mat"
             admin="Audie Raymos"
             discount={60}
         />
         <Deal 
             link="Link"
+            image={ss}
             title="Kitchen Spatula Set"
             admin="Stephanie Raymos"
             discount={75}
+        />
+        <Deal 
+            link="Link"
+            image={ts}
+            title="Kids toy tractor set"
+            admin="Stephanie Raymos"
+            discount={65}
         />
     </div>
   )
