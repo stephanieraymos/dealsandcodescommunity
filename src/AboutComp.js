0b919ece9c1e633bf9e479a06ce6af2b70ccb9c6
Stephanie Raymos <stephanieraymos@gmail.com>
2020-11-15 19:30:00 -0800

    About

---
+++ src/AboutComp.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import React from 'react'
+
+export default function AboutComp() {
+  return (
+    <div>
+      <h2>Welcome to Deals and Codes Community</h2>
+      <p>This site was created for our facebook group.</p>
+      <p>You'll find hundreds of amazing deals posted daily.</p>
+    </div>
+  )
+}
+
+
