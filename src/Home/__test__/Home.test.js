4f274d263c4663d0ecd168bf9bf78a598c987635
stephanieraymos <stephanieraymos@gmail.com>
2021-10-05 15:24:40 -0700

    Starting to implement test for home

---
+++ src/Home/__test__/Home.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Home from "../Home";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom-extend-expect"
+
+test("Header renders with correct text", () => {
+    
+})
+
