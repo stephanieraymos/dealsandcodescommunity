ee56c5ac92f3ba9f6e10983acc89055782cbbd14
stephanieraymos <stephanieraymos@gmail.com>
2021-10-05 14:56:37 -0700

    Post page

---
+++ src/Post.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Post = () => {
+    return (
+        <div>
+            {/* Text field for title and description
+            Image uploader 
+            Set up image previews based on link
+            Link */}
+        </div>
+    )
+}
+
+export default Post
