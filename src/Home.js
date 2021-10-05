f03cdf3b737ae2957a23ec9c19656b25bd3ab25f
stephanieraymos <stephanieraymos@gmail.com>
2021-10-05 15:08:51 -0700

    More info and link

--- src/Home.js
+++ src/Home.js
@@ -6,12 +6,17 @@ import ss from "./img/ss.jpg";
 import ts from "./img/ts.jpg";
 
 const Deal = ({ link, image, title, admin, discount }) => {
+    const moreInfo = () => {
+        // console.log("more info");
+        window.location.href = "#";
+    }
   return (
     <section className="deal">
       <p>{link}</p>
       <img src={image} alt={title} />
       <h2>{title}</h2>
       <p>Total discount: {discount}%</p>
+      <button onClick={ () => moreInfo()}>More info</button>
       <p>{admin}</p>
     </section>
   );
@@ -23,7 +28,8 @@ const DealsComp = () => {
       <h1 className="header">DEALS:</h1>
       <div className="deal-container">
         <Deal
-          link="Link"
+          link="Amazon"
+
           image={bottle}
           title="Water Bottle"
           admin="Stephanie Raymos"
