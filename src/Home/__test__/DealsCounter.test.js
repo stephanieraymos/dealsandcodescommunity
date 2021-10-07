15d237dc70d3b06040ef0bb3aa5910bf60d9299e
stephanieraymos <stephanieraymos@gmail.com>
2021-10-07 11:12:56 -0700

    Test for input

--- src/Home/__test__/DealsCounter.test.js
+++ src/Home/__test__/DealsCounter.test.js
@@ -5,4 +5,14 @@ import DealsCounter from "../DealsCounter";
 
 test("Deal count initially starts with text of 0", () => {
   const { getByTestId } = render(<DealsCounter />);
+  const counterEl = getByTestId("deal-counter");
+
+  expect(counterEl.textContent).toBe("0");
 });
+
+test("Input contains initial value of 1", () => {
+  const { getByTestId } = render(<DealsCounter />);
+  const inputEl = getByTestId("input");
+
+  expect(inputEl.value).toBe("1");
+})
