1f7b862e3f35a8fab01674a0ea8d4696bd7588a0
stephanieraymos <stephanieraymos@gmail.com>
2021-10-07 11:05:35 -0700

    Showing old way for docs

--- src/Home/__test__/Home.test.js
+++ src/Home/__test__/Home.test.js
@@ -4,6 +4,8 @@ import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
 test("Header renders with correct text", () => {
+    // const component = render(<Home />);
+    // const headerEl = component.getByTestId("header");
   const { getByTestId } = render(<Home />);
   const headerEl = getByTestId("header");
 
