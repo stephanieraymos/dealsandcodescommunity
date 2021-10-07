49ac2770954a1a9afd4d5a70c8deec459bfe4de5
stephanieraymos <stephanieraymos@gmail.com>
2021-10-07 11:26:25 -0700

    Set up routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Home from './Home/Home';
+import DealsCounter from './Home/DealsCounter';
+import Deals from './Home/Deals';
+// import Deal from './Home/Deal'
 
 function App() {
   return (
-    <Home />
+    <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/deals" component={Deals} />
+      <Route path="/deals-counter" component={DealsCounter} />
+    </Switch>
+  </Router>
   );
 }
 
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom"
 import Deals from "./Deals"
 
 const Home = () => {
   return (
     <div>
       <h1 data-testid="header">Test</h1>
+      <Link to="deals-counter">Deals Count</Link>
       <Deals />
     </div>
   );
