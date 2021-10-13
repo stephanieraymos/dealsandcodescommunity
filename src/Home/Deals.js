5994691d64dc8eb161ad2e815be2d819d2419cc5
stephanieraymos <stephanieraymos@gmail.com>
2021-10-13 15:14:34 -0700

    Getting ready for image previews

--- src/Home/Deals.js
+++ src/Home/Deals.js
@@ -6,6 +6,10 @@ import ss from "../img/ss.jpg";
 import ts from "../img/ts.jpg";
 
 const DealsComp = () => {
+  const tit = <meta property="og:title" content="Test" />
+  const desc = <meta property="og:description" content="Desc" />
+  const img = <meta property="og:image" content="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" />
+  console.log("img", img)
     return (
       <>
         <h1 className="header">Deals</h1>
@@ -34,6 +38,13 @@ const DealsComp = () => {
           <Deal
             link="Link"
             image={ts}
+            title="Kids toy tractor set"
+            admin="Stephanie Raymos"
+            discount={65}
+          />
+          <Deal
+            link="Link"
+            image={img}
             title="Kids toy tractor set"
             admin="Stephanie Raymos"
             discount={65}
